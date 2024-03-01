ce0c6a56e529039101dab92f1c808d8ac6ada884
Pranavi <inkslinger5198@gmail.com>
2024-03-01 10:13:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // Set the document title for the main page
+    document.title = 'Standard Demand Partners';
+    
+    // Optionally, reset the title when the component unmounts
+    return () => {
+      document.title = 'Standard Demand Partners';
+    };
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
